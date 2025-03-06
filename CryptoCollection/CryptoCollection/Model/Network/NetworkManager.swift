5c2496ba191077f9dcd96329a3939ec31eb3bbb8
Taerogrammer <hyung54321@naver.com>
2025-03-06 21:35:33 +0900

    feat: 네트워크 예시 주석 추가

--- CryptoCollection/CryptoCollection/Model/Network/NetworkManager.swift
+++ CryptoCollection/CryptoCollection/Model/Network/NetworkManager.swift
@@ -24,3 +24,48 @@ final class NetworkManager {
             }
     }
 }
+
+// MARK: - 예시
+//NetworkManager.shared.getItem(
+//    api: UpbitRouter.getMarket(),
+//    type: [UpbitMarketResponse].self) { result in
+//        switch result {
+//        case .success(let success):
+//            print("--> ", success)
+//        case .failure(let failure):
+//            print("Error", failure)
+//        }
+//    }
+//
+//NetworkManager.shared.getItem(
+//    api: CoingeckoRouter.getCoinInformation(ids: "bitcoin"),
+//    type: [CoingeckoCoinResponse].self) { result in
+//        switch result {
+//        case .success(let success):
+//            print("0-> ", success)
+//        case .failure(let failure):
+//            print("error", failure)
+//        }
+//    }
+//
+//NetworkManager.shared.getItem(
+//    api: CoingeckoRouter.getTrending,
+//    type: CoingeckoTrendingResponse.self) { result in
+//        switch result {
+//        case .success(let success):
+//            print("sucess", success)
+//        case .failure(let failure):
+//            print("fail", failure)
+//        }
+//    }
+//
+//NetworkManager.shared.getItem(
+//    api: CoingeckoRouter.getSearch(query: "Bitcoin"),
+//    type: CoingeckoSearchResponse.self) { result in
+//        switch result {
+//        case .success(let success):
+//            print("=-", success)
+//        case .failure(let failure):
+//            print("error", failure)
+//        }
+//    }
