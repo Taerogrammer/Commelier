0974051aed19c7c1fccddd54ffa355083b3a866a
Taerogrammer <hyung54321@naver.com>
2025-03-06 21:36:07 +0900

    feat: TabBar 및 ViewController 생성

--- CryptoCollection/CryptoCollection/Present/Tabbar/TabBarController.swift
+++ CryptoCollection/CryptoCollection/Present/Tabbar/TabBarController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class TabBarController: UITabBarController {
+final class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureTabBar()
+        configureTabBarAppearance()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func configureTabBar() {
+        let tickerVC = TickerViewController()
+        let trendingVC = TrendingViewController()
+        let portfolioVC = PortfolioViewController()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        tickerVC.tabBarItem.title = "거래소"
+        tickerVC.tabBarItem.image = UIImage(systemName: "chart.line.uptrend.xyaxis")
+        trendingVC.tabBarItem.title = "코인정보"
+        trendingVC.tabBarItem.image = UIImage(systemName: "chart.bar.fill")
+        portfolioVC.tabBarItem.title = "포트폴리오"
+        portfolioVC.tabBarItem.image = UIImage(systemName: "star")
+
+        setViewControllers([tickerVC, trendingVC, portfolioVC], animated: true)
     }
-    */
 
+    private func configureTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = .white
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.tintColor = .customBlack
+    }
 }
