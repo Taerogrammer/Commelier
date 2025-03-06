097893d3dbd49c7140159d6f9760ee77c37911dc
Taerogrammer <hyung54321@naver.com>
2025-03-07 01:35:01 +0900

    feat: 5초 타이머 호출 구현

--- CryptoCollection/CryptoCollection/Present/Ticker/View/TickerViewController.swift
+++ CryptoCollection/CryptoCollection/Present/Ticker/View/TickerViewController.swift
@@ -6,7 +6,58 @@
 //
 
 import UIKit
+import SnapKit
+import RxCocoa
+import RxSwift
 
 final class TickerViewController: BaseViewController {
+    private let tableView = UITableView()
+    private let viewModel = TickerViewModel()
+    private var disposeBag = DisposeBag()
+
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    override func configureView() {
+    }
+
+    override func configureNavigation() {
+        let titleLabel = UILabel()
+        titleLabel.text = "거래소"
+        titleLabel.font = .boldSystemFont(ofSize: 16)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+    }
+
+    override func bind() {
+        let output = viewModel.transform(input: TickerViewModel.Input())
+
+        output.data
+            .subscribe(with: self) { owner, value in
+                print("값", value)
+            } onDisposed: { owner in
+                print("onDisposed")
+            }
+            .disposed(by: disposeBag)
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 타이머 다시 시작
+        print(#function)
+
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        // 타이머 해제
+        print(#function)
+    }
 
 }
--- CryptoCollection/CryptoCollection/Present/Ticker/ViewModel/TickerViewModel.swift
+++ CryptoCollection/CryptoCollection/Present/Ticker/ViewModel/TickerViewModel.swift
@@ -5,4 +5,60 @@
 //  Created by 김태형 on 3/6/25.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
+
+final class TickerViewModel: ViewModel {
+    private let disposeBag = DisposeBag()
+
+    struct Input {
+
+    }
+
+    struct Output {
+        let data: Observable<[UpbitMarketResponse]>
+    }
+
+    func transform(input: Input) -> Output {
+        let data = PublishRelay<[UpbitMarketResponse]>()
+        let timer = Observable<Int>.interval(.seconds(5), scheduler: ConcurrentDispatchQueueScheduler.init(qos: .background))
+
+
+        // TODO: 방법 고민해보기
+        /*
+         .timer 메서드로 구현하면 0초부터 시작은 하나 늦게 호출됨.
+         처음은 전자의 식으로 호출 후 5초 뒤부턴 timer 식 호출
+         */
+        NetworkManager.shared.getItem(
+            api: UpbitRouter.getMarket(),
+            type: [UpbitMarketResponse].self)
+        .subscribe(with: self) { owner, value in
+            data.accept(value)
+        } onFailure: { owner, error in
+            print("failed", APIError.unknownError)
+        } onDisposed: { owner in
+            print("onDisposed")
+        }
+        .disposed(by: disposeBag)
+
+        timer
+            .subscribe(with: self) { owner, value in
+                print("Timer:", value)
+                NetworkManager.shared.getItem(
+                    api: UpbitRouter.getMarket(),
+                    type: [UpbitMarketResponse].self)
+                .subscribe(with: self) { owner, value in
+                    data.accept(value)
+                } onFailure: { owner, error in
+                    print("failed", error)
+                } onDisposed: { owner in
+                    print("onDisposed")
+                }
+                .disposed(by: owner.disposeBag)
+            }
+            .disposed(by: disposeBag)
+
+        return Output(data: data.asObservable())
+    }
+
+}
