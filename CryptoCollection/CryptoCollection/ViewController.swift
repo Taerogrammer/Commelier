341a169d3707b78c04b86f3dd0f2ea153e59f772
Taerogrammer <hyung54321@naver.com>
2025-03-06 19:00:22 +0900

    setting: 프로젝트 세팅

---
+++ CryptoCollection/CryptoCollection/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CryptoCollection
+//
+//  Created by 김태형 on 3/6/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+        view.backgroundColor = .blue
+    }
+}
