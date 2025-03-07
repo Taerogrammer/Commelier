565b293822713fb55cc6d14e03898197242d3c3f
Taerogrammer <hyung54321@naver.com>
2025-03-07 21:37:29 +0900

    feat: searchBar, navigationTitle 수정

--- CryptoCollection/CryptoCollection/Present/Trending/View/TrendingViewController.swift
+++ CryptoCollection/CryptoCollection/Present/Trending/View/TrendingViewController.swift
@@ -6,8 +6,48 @@
 //
 
 import UIKit
+import SnapKit
+import RxCocoa
+import RxDataSources
+import RxSwift
 
 final class TrendingViewController: BaseViewController {
+    private let searchBar = UISearchBar()
 
+
+    override func configureHierarchy() {
+        view.addSubview(searchBar)
+    }
+
+    override func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    override func configureView() {
+        configureSearchBar()
+    }
+
+    override func configureNavigation() {
+        let titleLabel = UILabel()
+        titleLabel.text = "가상자산 / 심볼 검색"
+        titleLabel.font = .boldSystemFont(ofSize: 16)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+    }
+
+    private func configureSearchBar() {
+        searchBar.clipsToBounds = true
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = "검색어를 입력해주세요"
+        searchBar.searchTextField.font = .systemFont(ofSize: 12)
+        searchBar.setImage(UIImage(systemName: "xmark"), for: .clear, state: .normal)
+        searchBar.tintColor = .customGray
+        searchBar.searchTextField.borderStyle = .none
+        searchBar.searchTextField.layer.borderWidth = 1
+        searchBar.searchTextField.layer.borderColor = UIColor.customGray.cgColor
+        searchBar.searchTextField.layer.cornerRadius = 16
+        searchBar.searchTextField.backgroundColor = .white
+    }
 
 }
