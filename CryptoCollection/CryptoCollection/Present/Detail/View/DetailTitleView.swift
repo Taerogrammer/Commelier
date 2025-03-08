59c59473c7692174a63011b83f207d030e1ab491
Taerogrammer <hyung54321@naver.com>
2025-03-08 20:30:45 +0900

    feat: 네비게이션 타이틀 이름 구현

---
+++ CryptoCollection/CryptoCollection/Present/Detail/View/DetailTitleView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailTitleView.swift
+//  CryptoCollection
+//
+//  Created by 김태형 on 3/8/25.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailTitleView: BaseView {
+    private let stackView = UIStackView()
+    let image = UIImageView()
+    let id = UILabel()
+
+    override func configureHierarchy() {
+        addSubview(stackView)
+
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .center
+        stackView.addArrangedSubview(image)
+        stackView.addArrangedSubview(id)
+    }
+
+    override func configureLayout() {
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        image.snp.makeConstraints { make in
+            make.size.equalTo(28)
+        }
+    }
+
+    override func configureView() {
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFit
+        image.layer.cornerRadius = 14
+        id.font = .boldSystemFont(ofSize: 16)
+        id.textColor = UIColor.customBlack
+    }
+}
--- CryptoCollection/CryptoCollection/Present/Detail/View/DetailViewController.swift
+++ CryptoCollection/CryptoCollection/Present/Detail/View/DetailViewController.swift
@@ -6,18 +6,49 @@
 //
 
 import UIKit
+import Kingfisher
 import RxCocoa
 import RxSwift
 import SnapKit
 
 final class DetailViewController: BaseViewController {
-    let viewModel: DetailViewModel
+    private let viewModel: DetailViewModel
+    private let disposeBag = DisposeBag()
+    private let titleView = DetailTitleView()
+
+    override func configureHierarchy() {
+
+    }
+
+    override func configureLayout() {
+
+    }
 
     override func configureView() {
+        bind()
+    }
+
+    override func configureNavigation() {
+
     }
 
     init(viewModel: DetailViewModel) {
         self.viewModel = viewModel
         super.init()
+    }
+
+    private func bind() {
+        let input = DetailViewModel.Input()
+        let output = viewModel.transform(input: input)
+
+        output.data
+            .withLatestFrom(output.data)
+            .bind(with: self) { owner, value in
+                owner.titleView.image.kf.setImage(with: URL(string: value.first!.image))
+                owner.titleView.id.text = value.first?.symbol.uppercased()
+                owner.navigationItem.titleView = owner.titleView
+            }
+            .disposed(by: disposeBag)
+
     }
 }
