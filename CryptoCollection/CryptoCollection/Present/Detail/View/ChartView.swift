d2b0dcb377d6f7edc294019a00a3c309e55adf0f
Taerogrammer <hyung54321@naver.com>
2025-03-09 02:06:29 +0900

    feat: 차트뷰 레이아웃 구성

---
+++ CryptoCollection/CryptoCollection/Present/Detail/View/ChartView.swift
@@ -0,0 +1,8 @@
+//
+//  ChartView.swift
+//  CryptoCollection
+//
+//  Created by 김태형 on 3/9/25.
+//
+
+import Foundation
--- CryptoCollection/CryptoCollection/Present/Detail/View/DetailChartView.swift
+++ CryptoCollection/CryptoCollection/Present/Detail/View/DetailChartView.swift
@@ -6,11 +6,65 @@
 //
 
 import UIKit
+import SwiftUI
+import SnapKit
 
 final class DetailChartView: BaseView {
+    let moneyLabel = UILabel()
+    let rateLabel = UILabel()
+    let updateDateLabel = UILabel()
+    var chartsHostingController: UIHostingController<ChartView>?
 
-    override func configureView() {
-        backgroundColor = .customGray
+    override func configureHierarchy() {
+        [moneyLabel, rateLabel, updateDateLabel].forEach { addSubview($0) }
     }
 
+    override func configureLayout() {
+        moneyLabel.snp.makeConstraints { make in
+            make.leading.equalTo(safeAreaLayoutGuide).inset(12)
+            make.top.equalTo(safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(32)
+        }
+        rateLabel.snp.makeConstraints { make in
+            make.leading.equalTo(moneyLabel)
+            make.top.equalTo(moneyLabel.snp.bottom).offset(4)
+            make.height.equalTo(14)
+        }
+        updateDateLabel.snp.makeConstraints { make in
+            make.leading.equalTo(moneyLabel)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(8)
+            make.height.equalTo(12)
+        }
+    }
+
+    override func configureView() {
+        configureChartHostingView()
+        moneyLabel.font = .boldSystemFont(ofSize: 26)
+        rateLabel.font = .boldSystemFont(ofSize: 12)
+        updateDateLabel.font = .systemFont(ofSize: 8)
+
+        moneyLabel.text = "$140,123,123"
+        rateLabel.text = "^0.98%"
+        updateDateLabel.text = "2/15 18:00:45 업데이트"
+    }
+
+}
+
+// MARK: - Chart
+extension DetailChartView {
+    private func configureChartHostingView() {
+//        let chartView = ChartView()
+        let chartView = ChartView()
+        let controller = UIHostingController(rootView: chartView)
+        self.chartsHostingController = controller
+
+        guard let chartUIView = controller.view else { return }
+        addSubview(chartUIView)
+
+        chartUIView.snp.makeConstraints { make in
+            make.top.equalTo(rateLabel.snp.bottom).inset(-12)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(12)
+            make.bottom.equalTo(updateDateLabel.snp.top).inset(-8)
+        }
+    }
 }
