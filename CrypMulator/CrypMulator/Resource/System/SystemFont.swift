996926cfe3da07f3fff084f66e7525c9d6f213c1
Taerogrammer <hyung54321@naver.com>
2025-03-25 21:53:02 +0900

    feat: SystemFont, SystemImage enum 구현

---
+++ CrypMulator/CrypMulator/Resource/System/SystemFont.swift
@@ -0,0 +1,17 @@
+//
+//  SystemFont.swift
+//  CrypMulator
+//
+//  Created by 김태형 on 3/25/25.
+//
+
+import UIKit
+
+enum SystemFont {
+    static let title1 = UIFont.systemFont(ofSize: 28, weight: .bold)
+    static let title2 = UIFont.systemFont(ofSize: 24, weight: .semibold)
+    static let subtitle = UIFont.systemFont(ofSize: 18, weight: .medium)
+    static let body = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let caption = UIFont.systemFont(ofSize: 12, weight: .regular)
+    static let tab = UIFont.systemFont(ofSize: 12, weight: .medium)
+}
