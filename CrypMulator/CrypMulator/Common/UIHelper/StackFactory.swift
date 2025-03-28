9ec39ddff8ba11836c55ce2283d3472cbc3eeb6c
Taerogrammer <hyung54321@naver.com>
2025-03-28 13:54:09 +0900

    feat: StackView 관련 메서드 구현

---
+++ CrypMulator/CrypMulator/Common/UIHelper/StackFactory.swift
@@ -0,0 +1,36 @@
+//
+//  StackFactory.swift
+//  CrypMulator
+//
+//  Created by 김태형 on 3/28/25.
+//
+
+import UIKit
+
+enum StackFactory {
+
+    static func horizontalRowStack(
+        left: UILabel,
+        right: UILabel,
+        spacing: CGFloat = 4,
+        distribution: UIStackView.Distribution = .fillEqually
+    ) -> UIStackView {
+        let stack = UIStackView(arrangedSubviews: [left, right])
+        stack.axis = .horizontal
+        stack.spacing = spacing
+        stack.distribution = distribution
+        return stack
+    }
+
+    static func verticalStack(
+        views: [UIView],
+        spacing: CGFloat = 8,
+        alignment: UIStackView.Alignment = .fill
+    ) -> UIStackView {
+        let stack = UIStackView(arrangedSubviews: views)
+        stack.axis = .vertical
+        stack.spacing = spacing
+        stack.alignment = alignment
+        return stack
+    }
+}
---
+++ CrypMulator/CrypMulator/Presentation/Portfolio/View/TradeHistory/TradeHistoryCell.swift
@@ -0,0 +1,12 @@
+//
+//  TradeHistoryCell.swift
+//  CrypMulator
+//
+//  Created by 김태형 on 3/28/25.
+//
+
+import Cocoa
+
+class TradeHistoryCell: UITableViewCell {
+
+}
