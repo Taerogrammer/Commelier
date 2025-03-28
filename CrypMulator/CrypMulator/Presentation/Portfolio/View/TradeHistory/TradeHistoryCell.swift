55d56e3965fb8abdcfdbead79f4cff0af5228edd
Taerogrammer <hyung54321@naver.com>
2025-03-28 14:13:12 +0900

    feat: 거래내역 뷰 구현

--- CrypMulator/CrypMulator/Presentation/Portfolio/View/TradeHistory/TradeHistoryCell.swift
+++ CrypMulator/CrypMulator/Presentation/Portfolio/View/TradeHistory/TradeHistoryCell.swift
@@ -5,8 +5,135 @@
 //  Created by 김태형 on 3/28/25.
 //
 
-import Cocoa
+import UIKit
+import SnapKit
 
-class TradeHistoryCell: UITableViewCell {
+final class TradeHistoryCell: BaseTableViewCell, ReuseIdentifiable {
 
+    private let typeLabel = UILabel()
+    private let pairLabel = UILabel()
+
+    private let leftStackView = UIStackView()
+    private let infoStackView = UIStackView()
+    private let containerStackView = UIStackView()
+
+    private let priceTitleLabel = UILabel()
+    private let priceValueLabel = UILabel()
+
+    private let amountTitleLabel = UILabel()
+    private let amountValueLabel = UILabel()
+
+    private let totalTitleLabel = UILabel()
+    private let totalValueLabel = UILabel()
+
+    private let dateTitleLabel = UILabel()
+    private let dateValueLabel = UILabel()
+
+    private let topLineView = BaseView()
+    private let bottomLineView = BaseView()
+
+    override func configureHierarchy() {
+        leftStackView.addArrangedSubviews([typeLabel, pairLabel])
+
+        infoStackView.addArrangedSubviews([
+            StackFactory.horizontalRowStack(left: priceTitleLabel, right: priceValueLabel),
+            StackFactory.horizontalRowStack(left: amountTitleLabel, right: amountValueLabel),
+            StackFactory.horizontalRowStack(left: totalTitleLabel, right: totalValueLabel),
+            StackFactory.horizontalRowStack(left: dateTitleLabel, right: dateValueLabel)
+        ])
+
+        containerStackView.addArrangedSubviews([leftStackView, infoStackView])
+        contentView.addSubviews([topLineView, containerStackView, bottomLineView])
+    }
+
+    override func configureLayout() {
+        topLineView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalToSuperview()
+            make.height.equalTo(0.5)
+        }
+        containerStackView.snp.makeConstraints { make in
+            make.top.equalTo(topLineView.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+            make.bottom.equalTo(bottomLineView.snp.top)
+        }
+
+        bottomLineView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(0.5)
+            make.bottom.equalToSuperview()
+        }
+    }
+
+    override func configureView() {
+        topLineView.backgroundColor = SystemColor.gray
+
+        typeLabel.font = SystemFont.Title.small
+        pairLabel.font = SystemFont.Body.small
+        pairLabel.textColor = SystemColor.gray
+
+        [priceTitleLabel, amountTitleLabel, totalTitleLabel, dateTitleLabel].forEach {
+            $0.font = SystemFont.Body.content
+            $0.textColor = SystemColor.black
+        }
+
+        [priceValueLabel, amountValueLabel, totalValueLabel, dateValueLabel].forEach {
+            $0.font = SystemFont.Body.content
+            $0.textAlignment = .right
+        }
+
+        dateValueLabel.textColor = SystemColor.gray
+
+        leftStackView.axis = .vertical
+        leftStackView.spacing = 4
+        leftStackView.alignment = .leading
+
+        infoStackView.axis = .vertical
+        infoStackView.spacing = 6
+        infoStackView.alignment = .fill
+
+        containerStackView.axis = .horizontal
+        containerStackView.spacing = 16
+        containerStackView.alignment = .center
+
+        bottomLineView.backgroundColor = SystemColor.gray
+    }
+}
+
+// MARK: - configure
+extension TradeHistoryCell {
+    func configure(with model: TradeHistoryModel) {
+        typeLabel.text = model.type == .buy ? "매수" : "매도"
+        typeLabel.textColor = model.type == .buy ? SystemColor.red : SystemColor.blue
+
+        pairLabel.text = model.pair
+
+        priceTitleLabel.text = "체결 가격"
+        priceValueLabel.text = model.price
+
+        amountTitleLabel.text = "체결 수량"
+        amountValueLabel.text = model.amount
+        amountValueLabel.textColor = model.type == .buy ? SystemColor.red : SystemColor.blue
+
+        totalTitleLabel.text = "거래 금액"
+        totalValueLabel.text = model.total
+
+        dateTitleLabel.text = "체결 일시"
+        dateValueLabel.text = model.date
+    }
+
+}
+
+enum TradeType {
+    case buy
+    case sell
+}
+
+struct TradeHistoryModel {
+    let type: TradeType
+    let pair: String
+    let price: String
+    let amount: String
+    let total: String
+    let date: String
 }
--- CrypMulator/CrypMulator/Presentation/Portfolio/View/TradeHistory/TradeHistoryViewController.swift
+++ CrypMulator/CrypMulator/Presentation/Portfolio/View/TradeHistory/TradeHistoryViewController.swift
@@ -10,4 +10,53 @@ import SnapKit
 
 final class TradeHistoryViewController: BaseViewController {
 
+    private let tableView = UITableView()
+    private var data: [TradeHistoryModel] = []
+
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    override func configureLayout() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    override func configureView() {
+        title = "거래내역"
+        view.backgroundColor = .white
+
+        tableView.register(TradeHistoryCell.self, forCellReuseIdentifier: TradeHistoryCell.identifier)
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+
+        tableView.rowHeight = 120
+
+        setupData()
+    }
+
+    private func setupData() {
+        data = [
+            TradeHistoryModel(type: .buy, pair: "KRW-BTC", price: "1,123,123 KRW", amount: "0.0125 ETH", total: "124,532,51 KRW", date: "2024.02.20 10:45"),
+            TradeHistoryModel(type: .sell, pair: "KRW-BTC", price: "1,123,123 KRW", amount: "0.0125 ETH", total: "124,532,51 KRW", date: "2024.02.20 10:45"),
+            TradeHistoryModel(type: .sell, pair: "KRW-BTC", price: "1,123,123 KRW", amount: "0.0125 ETH", total: "124,532,51 KRW", date: "2024.02.20 10:45"),
+            TradeHistoryModel(type: .sell, pair: "KRW-BTC", price: "1,123,123 KRW", amount: "0.0125 ETH", total: "124,532,51 KRW", date: "2024.02.20 10:45")
+        ]
+        tableView.reloadData()
+    }
+}
+
+extension TradeHistoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TradeHistoryCell.identifier, for: indexPath) as? TradeHistoryCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: data[indexPath.row])
+        return cell
+    }
 }
